f8bf70cffb223b8fa5d6ec36d35fc86e9cc7a228
Ricardo La Rosa <rilaros@gmail.com>
2021-02-18 08:04:38 -0500

    Added get data from API and loader

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-import logo from "../images/logo.png";
 import "../index.css";
-import data from "../data.json";
+import logo from "../images/logo.png";
+
+import Loader from "./Loader";
 
 function CharacterCard(props) {
   const { character } = props;
@@ -20,25 +21,71 @@ function CharacterCard(props) {
 }
 
 class App extends React.Component {
+  state = {
+    nextPage: 1,
+    loading: true,
+    error: null,
+    data: {
+      results: [],
+    },
+  };
+
+  componentDidMount() {
+    this.fetchCharacters();
+  }
+
+  fetchCharacters = async () => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/?page=${this.state.nextPage}`
+      );
+      const data = await response.json();
+
+      this.setState({
+        loading: false,
+        data: {
+          info: data.info,
+          results: [].concat(this.state.data.results, data.results),
+        },
+        nextPage: this.state.nextPage + 1,
+      });
+    } catch (error) {
+      this.setState({
+        loading: false,
+        error: error,
+      });
+    }
+  };
+
   render() {
+    if (this.state.error) {
+      return `Error: ${this.state.error.message}`;
+    }
+
     return (
       <div className="container">
         <div className="App">
           <img className="Logo" src={logo} alt="Rick y Morty" />
 
           <ul className="row">
-            {data.results.map((character) => (
+            {this.state.data.results.map((character) => (
               <li className="col-6 col-md-3" key={character.id}>
                 <CharacterCard character={character} />
               </li>
             ))}
           </ul>
 
-          {/* {this.state.loading && <p className="text-center">Loading...</p>} */}
-          {/* 
+          {this.state.loading && (
+            <div className="loader">
+              <Loader />
+            </div>
+          )}
+
           {!this.state.loading && this.state.data.info.next && (
             <button onClick={() => this.fetchCharacters()}>Load More</button>
-          )} */}
+          )}
         </div>
       </div>
     );
---
+++ src/components/Loader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Loader extends React.Component {
+  render() {
+    return <p className="text-center">Loading...</p>;
+  }
+}
+
+export default Loader;
